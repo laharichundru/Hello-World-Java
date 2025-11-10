4469caa00c6a277f2276d1c9e54bd1a984aa429a
chundru.naga.lahari <chundru.naga.lahari@bdc11-l-530406w.dir.svc.accenture.com>
2025-11-10 21:24:34 +0530

    Through Git stagging

--- src/Java/HellowWorld.java
+++ src/Java/HellowWorld.java
@@ -4,10 +4,11 @@ public class HellowWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- System.out.println("Hello World..!");
+         System.out.println("Hello World..!");
 		 System.out.println("Hello World..!!!");
 		 System.out.println("push again..args.!!!");
-	}
-		
+		 System.out.println("Learning git push from git stagging");
+
+}
 }
 
