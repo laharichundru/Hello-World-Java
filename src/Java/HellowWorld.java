82709696c2bc16277776a52cea8a4bf183fab906
laharichundru <chundrunagalahari@gmail.com>
2025-11-12 20:51:47 +0530

    Update HellowWorld.java

--- src/Java/HellowWorld.java
+++ src/Java/HellowWorld.java
@@ -10,7 +10,9 @@ public class HellowWorld {
 		 System.out.println("Learning git push from git stagging");
 		 System.out.println("learning git push and git pull commands using local git");
 		 System.out.println("added one more line");
+		System.out.println("lets learn git pull using git bash");
 
 }
 }
+
 
